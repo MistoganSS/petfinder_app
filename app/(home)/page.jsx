f74b9fb0abfddf16336bcbd6080d053bc98fb6db
Jilmer Roca <jilmer.roca@gmail.com>
2024-07-10 22:08:59 -0500

    Changes in HomePage

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import PetForm from '../components/PetForm'
 import { animalList } from '../mocks/animalsList'
+import { TbHeart, TbShare, TbCirclePlus, TbBookmark, TbMapSearch, TbZoomMoney } from "react-icons/tb";
 
-export default function Home () {
+export default function Home() {
   return (
     <main className='lg:p-10 p-5 pt-24 flex flex-col gap-20'>
       <section className='flex flex-col lg:flex-row justify-between items-center gap-5 p-5'>
@@ -43,6 +44,129 @@ export default function Home () {
           <p>Reach More People In Your Local Area</p>
         </div>
       </section>
+
+      <section class="bg-gray-50 py-8 antialiased md:py-12">
+        <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+          <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
+            <div>
+              <h2 class="mt-3 text-xl font-semibold text-gray-900 sm:text-2xl">Pet List</h2>
+            </div>
+            <div class="flex items-center space-x-4">
+              <button data-modal-toggle="filterModal" data-modal-target="filterModal" type="button" class="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto">
+                <svg class="-ms-0.5 me-2 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z" />
+                </svg>
+                Filters
+                <svg class="-me-0.5 ms-2 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
+                </svg>
+              </button>
+              <button id="sortDropdownButton1" data-dropdown-toggle="dropdownSort1" type="button" class="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto">
+                <svg class="-ms-0.5 me-2 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M7 4l3 3M7 4 4 7m9-3h6l-6 6h6m-6.5 10 3.5-7 3.5 7M14 18h4" />
+                </svg>
+                Sort
+                <svg class="-me-0.5 ms-2 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7" />
+                </svg>
+              </button>
+              <div id="dropdownSort1" class="z-50 hidden w-40 divide-y divide-gray-100 rounded-lg bg-white shadow" data-popper-placement="bottom">
+                <ul class="p-2 text-left text-sm font-medium text-gray-500" aria-labelledby="sortDropdownButton">
+                  <li>
+                    <a href="#" class="group inline-flex w-full items-center rounded-md px-3 py-2 text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-900"> The most recent </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
+
+
+            {animalList.map(item => {
+              return (
+                <article key={item.id} class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
+                  <div class="h-56 w-full">
+                    <a href="#">
+                      <img class="mx-auto h-full" src={item.photos} alt="" />
+                    </a>
+                  </div>
+                  <div class="pt-6">
+                    <div class="mb-4 flex items-center justify-between gap-4">
+                      {(() => {
+                        if (item.status === 'Lost') {
+                          return (
+                            <span class="uppercase me-2 rounded bg-red-100 px-2.5 py-0.5 text-xs font-bold text-red-800">
+                              {item.status}
+                            </span>
+                          )
+                        } else if (item.status === 'Found') {
+                          return (
+                            <span class="uppercase me-2 rounded bg-green-100 px-2.5 py-0.5 text-xs font-bold text-green-800">
+                              {item.status}
+                            </span>
+                          )
+                        } else {
+                          return (
+                            <span class="uppercase me-2 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-bold text-blue-800">
+                              {item.status}
+                            </span>
+                          )
+                        }
+                      })()}
+                      <div class="flex items-center justify-end gap-1">
+                        <span className='text-gray-500'>S/ {item.reward}</span>
+                        <button type="button" data-tooltip-target="tooltip-add-to-favorites" class="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900">
+                          <span class="sr-only"> Add to Bookmark </span><TbBookmark className='h-5 w-5' />
+                        </button>
+                      </div>
+                    </div>
+                    <a href="#" class="text-lg font-semibold leading-tight text-gray-900 hover:underline">
+                      {item.name} (Dog)
+                    </a>
+                    <p>
+                      {item.description}
+                    </p>
+                    <div class="mt-2 flex items-center gap-2">
+                      <p class="text-sm font-medium text-gray-900">Seen {item.dateLastSeen}</p>
+                      <p class="text-sm font-medium text-gray-500">(45d ago)</p>
+                    </div>
+                    <ul class="mt-2 flex items-center gap-4">
+                      <li class="flex items-center gap-2">
+                        <p class="text-sm font-small text-gray-500">
+                          {item.areaLastSeen}, {item.nearestLandmark}, {item.crossStreet}
+                        </p>
+                      </li>
+                      <li>
+                        <button href={`https://www.google.com/maps/search/${item.ddLat}+${item.ddLon}`} class="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900">
+                          <span class="sr-only"> Localizar </span><TbMapSearch className='h-4 w-4' />
+                        </button>
+                      </li>
+                    </ul>
+                    <div class="mt-4 flex items-center justify-between gap-4">
+                      <p class="text-2xl font-extrabold leading-tight text-gray-900 hidden">$1,699</p>
+                      <button type="button" class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
+                        <TbCirclePlus className='-ms-2 me-2 h-5 w-5' />
+                        Details
+                      </button>
+                      <button type="button" class="inline-flex items-center rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100">
+                        <TbShare className='-ms-2 me-2 h-5 w-5' />
+                        Share
+                      </button>
+                    </div>
+                  </div>
+                </article>
+              )
+            })}
+          </div>
+          <div class="w-full text-center">
+            <button type="button" class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100">
+              Show more
+            </button>
+          </div>
+        </div>
+      </section>
+
+
       <section className='flex flex-col items-end'>
         <div className='max-w-screen-lg'>
           <h2 className='text-right text-5xl mb-5 font-bold'>
@@ -56,13 +180,16 @@ export default function Home () {
             neighbors.
           </p>
         </div>
+
+
+
         <div className='py-5 grid grid-cols-2 lg:grid-cols-5 gap-3'>
           {animalList.map(item => {
             return (
               <article key={item.id} className='bg-red-600 rounded-lg'>
                 <div className='p-5'>
                   <img src={item.photos} alt='' />
-                  <p className='font-bold'>{item.name}</p>
+                  <h3 className='font-bold'>{item.name}</h3>
                   <p className=''>
                     {item.areaLastSeen}, {item.nearestLandmark}
                   </p>
@@ -84,14 +211,14 @@ export default function Home () {
           <div className='w-full'>
             <label
               htmlFor='name'
-              className='block mb-2 text-lg font-bold text-gray-900 dark:text-white'
+              className='block mb-2 text-lg font-bold text-gray-900'
             >
               Name
             </label>
             <div className='relative w-full'>
               <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
                 <svg
-                  className='w-4 h-4 text-gray-500 dark:text-gray-400'
+                  className='w-4 h-4 text-gray-500'
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
@@ -119,14 +246,14 @@ export default function Home () {
           <div className='w-full'>
             <label
               htmlFor=''
-              className='block mb-2 text-lg font-bold text-gray-900 dark:text-white'
+              className='block mb-2 text-lg font-bold text-gray-900'
             >
               Select an option
             </label>
             <div className='relative w-full'>
               <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
                 <svg
-                  className='w-4 h-4 text-gray-500 dark:text-gray-400'
+                  className='w-4 h-4 text-gray-500'
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
@@ -155,14 +282,14 @@ export default function Home () {
           <div className='w-full'>
             <label
               htmlFor='location'
-              className='block mb-2 text-lg font-bold text-gray-900 dark:text-white'
+              className='block mb-2 text-lg font-bold text-gray-900'
             >
               Location
             </label>
             <div className='relative w-full'>
               <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
                 <svg
-                  className='w-4 h-4 text-gray-500 dark:text-gray-400'
+                  className='w-4 h-4 text-gray-500'
                   aria-hidden='true'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
@@ -181,7 +308,7 @@ export default function Home () {
                 type='text'
                 id='location'
                 name='location'
-                className='ps-8 bg-transparent placeholder-slate-500 border-none text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5  dark:shadow-sm-light'
+                className='ps-8 bg-transparent placeholder-slate-500 border-none text-gray-900 text-sm rounded-lg focus:outline-none block w-full p-2.5'
                 placeholder='Type Location'
                 required
               />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,45 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: { "50": "#fffbeb", "100": "#fef3c7", "200": "#fde68a", "300": "#fcd34d", "400": "#fbbf24", "500": "#f59e0b", "600": "#d97706", "700": "#b45309", "800": "#92400e", "900": "#78350f", "950": "#451a03" }
+      },
+    },
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
     },
   },
   plugins: [],
